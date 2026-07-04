41b57ac0055631032bbe8abfd205ad67bf2ac41b
Carl Stone <carl.j.stone@vanderbilt.edu>
2026-07-04 01:45:17 -0500

    finish initial heatmap

---
+++ R/themes.R
@@ -0,0 +1,2 @@
+#' @export
+palette_dotplot_pair <- c("#2166ac", "#e31a8c")
---
+++ data-raw/umap-feature-list.R
@@ -0,0 +1,17 @@
+umap_feature_list <- c(
+  "Rlbp1",
+  "Hes1",
+  "Hes6",
+  "Ascl1",
+  "Neurog2",
+  "Otx2",
+  "Cabp5",
+  "Crx",
+  "Rho"
+)
+
+save(
+  umap_feature_list,
+  file = "data/umap_feature_list.rda",
+  version = 2
+)
